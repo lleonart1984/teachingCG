4a143b0dc586034eb1a3a02c4464745bf0041473
Carmen I Cabrera <kr1000a@gmail.com>
2021-03-23 17:50:56 -0400

    Fill the union and base of the coffee maker

--- Classes/UH2021/CCREMG/Renderer/Program.cs
+++ Classes/UH2021/CCREMG/Renderer/Program.cs
@@ -11,22 +11,11 @@ namespace Renderer
         static void Main(string[] args)
         {
             Raster render = new Raster(1024, 512);
-            // FreeTransformTest(render);
-            // DrawRoomTest(render);
             CofeeMakerTest(render);
             render.RenderTarget.Save("test.rbm");
             Console.WriteLine("Done.");
         }
 
-        public static float3[] RandomPositionsInBoxSurface(int N)
-        {
-            float3[] points = new float3[N];
-
-            for (int i = 0; i < N; i++)
-                points[i] = randomInBox();
-
-            return points;
-        }
 
         public static float3[] ApplyTransform(float3[] points, float4x4 matrix)
         {
@@ -55,87 +44,8 @@ namespace Renderer
             return result;
         }
 
-        private static void FreeTransformTest(Raster render)
-        {
-            render.ClearRT(float4(0, 0, 0.2f, 1)); // clear with color dark blue.
-
-            int N = 100000;
-            // Create buffer with points to render
-            float3[] points = RandomPositionsInBoxSurface(N);
-
-            // Creating boxy...
-            points = ApplyTransform(points, float4x4(
-                1f, 0, 0, 0,
-                0, 1.57f, 0, 0,
-                0, 0, 1f, 0,
-                0, 0, 0, 1
-                ));
-
-            // Apply a free transform
-            points = ApplyTransform(points, p => float3(p.x * cos(p.y) + p.z * sin(p.y), p.y, p.x * sin(p.y) - p.z * cos(p.y)));
-
-            #region viewing and projecting
-
-            points = ApplyTransform(points, Transforms.LookAtLH(float3(5f, 2.6f, 4), float3(0, 0, 0), float3(0, 1, 0)));
-            points = ApplyTransform(points, Transforms.PerspectiveFovLH(pi_over_4, render.RenderTarget.Height / (float)render.RenderTarget.Width, 0.01f, 10));
-
-            #endregion
-
-            render.DrawPoints(points);
-        }
-
-        public static void DrawRoomTest(Raster raster)
-        {
-            raster.ClearRT(float4(0, 0, 0.2f, 1)); // clear with color dark blue.
-
-            int N = 100000;
-            // Create buffer with points to render
-            float3[] points = RandomPositionsInBoxSurface(N);
-
-            float4x4 viewMatrix = Transforms.LookAtLH(float3(5f, 4.6f, 2), float3(0, 0, 0), float3(0, 1, 0));
-            float4x4 projMatrix = Transforms.PerspectiveFovLH(pi_over_4, raster.RenderTarget.Height / (float)raster.RenderTarget.Width, 0.01f, 10);
-
-            DrawRoom(raster, points, mul(viewMatrix, projMatrix));
-        }
-
-        private static void DrawRoom(Raster raster, float3[] boxPoints, float4x4 transform)
-        {
-            DrawTable(raster, boxPoints, mul(Transforms.Translate(0, 0, 0), transform));
-            DrawTable(raster, boxPoints, mul(Transforms.RotateRespectTo(float3(1,0,0), float3(0,1,0), pi/2), transform));
-        }
-
-        private static void DrawTable (Raster raster, float3[] boxPoints, float4x4 transform)
-        {
-            DrawTableLeg(raster, boxPoints, mul(Transforms.Translate(0.2f,0,0.2f), transform));
-            DrawTableLeg(raster, boxPoints, mul(Transforms.Translate(1.6f,0,0.2f), transform));
-            DrawTableLeg(raster, boxPoints, mul(Transforms.Translate(1.6f,0,1.6f), transform));
-            DrawTableLeg(raster, boxPoints, mul(Transforms.Translate(0.2f,0,1.6f), transform));
-            DrawTableTop(raster, boxPoints, mul(Transforms.Translate(0, 2, 0), transform));
-        }
-        
-        private static void DrawTableTop(Raster raster, float3[] boxPoints, float4x4 transform)
-        {
-            float4x4 transformingIntoLeg = mul(Transforms.Scale(2.2f, 0.2f, 2.2f), transform);
-            DrawBox(raster, boxPoints, transformingIntoLeg);
-        }
-
-        private static void DrawTableLeg(Raster raster, float3[] boxPoints, float4x4 transform)
-        {
-            float4x4 transformingIntoLeg = mul(Transforms.Scale(0.2f, 2, 0.2f), transform);
-            DrawBox(raster, boxPoints, transformingIntoLeg);
-        }
-
-        private static void DrawBox(Raster raster, float3[] boxPoints, float4x4 transform)
-        {
-            float4x4 transformingIntoBox = mul(float4x4(
-                0.5f, 0, 0, 0,
-                0, 0.5f, 0, 0,
-                0, 0, 0.5f, 0,
-                0.5f, 0.5f, 0.5f, 1
-                ), transform);
-
-            float3[] pointsToDraw = ApplyTransform(boxPoints, transformingIntoBox);
-            raster.DrawPoints(pointsToDraw);
+        private static void DrawCoffeeMaker(Raster render){
+            
         }
 
         private static void CofeeMakerTest(Raster render)
@@ -165,9 +75,11 @@ namespace Renderer
             List<float3> topBasePoints = PoliedroXZ(sides, float3(0, altura_base + h_base, 0), 1.3f);
 
             List<float3> basePoints = FillPoliedroXZ(buttonBasePoints, float3(0, 0, 0), cloud);
+            List<float3> coffeeBasePoints = FillCoffeeBase(buttonBasePoints, topBasePoints, cloud);
 
-            List<float3> buttonUnionPoints = PoliedroXZ(sides * 10, float3(0, altura_union, 0), 1.35f);
-            List<float3> topUnionPoints = PoliedroXZ(sides * 10, float3(0, altura_union + h_union, 0), 1.35f);
+            List<float3> unionPoints = DrawCylinder(float3(0, 0, 0), altura_union, h_union, 1.35f, cloud, random);
+            // List<float3> buttonUnionPoints = PoliedroXZ(sides * 10, float3(0, altura_union, 0), 1.35f);
+            // List<float3> topUnionPoints = PoliedroXZ(sides * 10, float3(0, altura_union + h_union, 0), 1.35f);
 
             List<float3> buttonTopPoints = PoliedroXZ(sides, float3(0, altura_tope, 0), 1.4f);
             List<float3> topTopPoints = PoliedroXZ(sides, float3(0, altura_tope + h_tope, 0), 2.1f);
@@ -184,7 +96,7 @@ namespace Renderer
 
             DownPointsList.AddRange(UnirPoliedros(buttonBasePoints, topBasePoints, cloud, random));
 
-            UpPointsList.AddRange(UnirPoliedros(buttonUnionPoints, topUnionPoints, 5, random));
+            // UpPointsList.AddRange(UnirPoliedros(buttonUnionPoints, topUnionPoints, 5, random));
             UpPointsList.AddRange(UnirPoliedros(buttonTopPoints, topTopPoints, cloud, random));
             UpPointsList.AddRange(UnirPoliedros(buttonTapaPoints, topTapaPoints, cloud, random));
             UpPointsList.AddRange(UnirPoliedros(buttonCositaPoints, topCositaPoints, cloud, random));
@@ -201,6 +113,8 @@ namespace Renderer
 
             DownPointsList.AddRange(UpPointsList);
             DownPointsList.AddRange(basePoints);
+            DownPointsList.AddRange(coffeeBasePoints);
+            DownPointsList.AddRange(unionPoints);
             float3[] points = DownPointsList.ToArray();
 
 
@@ -237,6 +151,17 @@ namespace Renderer
             points.AddRange(FillTriangleXZ(center, poli[0], poli[poli.Count - 1], cloud));
             return points;
         }
+        private static List<float3> FillCoffeeBase(List<float3> baseF, List<float3> topF, int cloud){
+            var points = new List<float3>();
+            for(int i = 1; i < baseF.Count; ++i){
+                points.AddRange(FillTriangleXZ(baseF[i - 1], baseF[i], topF[i - 1], cloud));
+                points.AddRange(FillTriangleXZ(baseF[i], topF[i], topF[i - 1], cloud));
+            }
+
+            points.AddRange(FillTriangleXZ(baseF[baseF.Count - 1], baseF[0], topF[topF.Count - 1], cloud));
+            points.AddRange(FillTriangleXZ(baseF[0], topF[0], topF[topF.Count - 1], cloud));
+            return points;
+        }
 
         private static List<float3> FillTriangleXZ(float3 a, float3 b, float3 c, int cloud)
         {
@@ -251,12 +176,31 @@ namespace Renderer
                 float3 item = (1 - sqrt(u)) * a;
                 item += (sqrt(u) * (1 - v)) * b;
                 item += v * sqrt(u) * c;
-                // Console.WriteLine(item);
                 points.Add(item);
             }
             return points;
         }
 
+        private static List<float3> DrawCylinder(float3 center, float baseHeight, float h, float r, int cloud, GRandom rnd){
+            var points = new List<float3>();
+            
+            while(cloud-- > 0){
+                float x = rnd.random();
+                x *= r;
+                
+                float y = rnd.random();
+                y *= h;
+                y += baseHeight;
+
+                float z = (float)Math.Sqrt(r * r - x * x);
+
+                points.Add(float3(x, y, z));
+                points.Add(float3(x, y, -z));
+                points.Add(float3(-x, y, z));
+                points.Add(float3(-x, y, -z));
+            }
+            return points;
+        }
 
         private static List<float3> PoliedroXZ(int sides, float3 centre, float radio)
         {
