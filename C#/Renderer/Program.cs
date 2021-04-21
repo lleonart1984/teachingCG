76aac4a45ca3f492ab8b27fe17a77185b72ee569
Ludwig Leonard <lleonart1984@gmail.com>
2021-04-21 08:10:31 +0200

    Added grid acceleration data-structure. And using BRDF for local lighting.

--- C#/Renderer/Program.cs
+++ C#/Renderer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using GMath;
 using Rendering;
 using System;
+using System.Diagnostics;
 using static GMath.Gfx;
 
 namespace Renderer
@@ -195,15 +196,48 @@ namespace Renderer
             
             /// Creates the model using a revolution of a bezier.
             /// Only Positions are updated.
-            var model = Manifold<PositionNormal>.Revolution(10, 10, t => EvalBezier(contourn, t), float3(0, 1, 0)).Weld();
+            var model = Manifold<PositionNormal>.Revolution(30, 20, t => EvalBezier(contourn, t), float3(0, 1, 0)).Weld();
             model.ComputeNormals();
             return model;
         }
 
+        static Mesh<PositionNormal> CreateSea()
+        {
+            /// Creates the model using a revolution of a bezier.
+            /// Only Positions are updated.
+            var model = Manifold<PositionNormal>.Surface(30, 20, (u, v) => 2*float3(2 * u - 1, sin(u*15)*0.02f+cos(v*13 + u*16)*0.03f, 2 * v - 1)).Weld();
+            model.ComputeNormals();
+            return model;
+        }
+
+        public delegate float3 BRDF(float3 N, float3 Lin, float3 Lout);
+
+        static BRDF LambertBRDF(float3 diffuse)
+        {
+            return (N, Lin, Lout) => diffuse / pi;
+        }
+
+        static BRDF BlinnBRDF(float3 specular, float power)
+        {
+            return (N, Lin, Lout) =>
+            {
+                float3 H = normalize(Lin + Lout);
+                return specular * pow(max(0, dot(H, N)), power) * (power + 2) / two_pi;
+            };
+        }
+
+        static BRDF Mixture(BRDF f1, BRDF f2, float alpha)
+        {
+            return (N, Lin, Lout) => lerp(f1(N, Lin, Lout), f2(N, Lin, Lout), alpha);
+        }
+
         static void CreateMeshScene(Scene<PositionNormal> scene)
         {
-            var model = CreateModel();
-            scene.Add(model.AsRaycast(), Transforms.Identity);
+            var sea = CreateSea();
+            scene.Add(sea.AsRaycast(RaycastingMeshMode.Grid), Transforms.Identity);
+
+            var egg = CreateModel();
+            scene.Add(egg.AsRaycast(RaycastingMeshMode.Grid), Transforms.Translate(0, 0.5f, 0));
         }
 
 
@@ -212,12 +246,20 @@ namespace Renderer
             // Scene Setup
             float3 CameraPosition = float3(3, 2f, 4);
             float3 LightPosition = float3(3, 5, -2);
+            float3 LightIntensity = float3(1, 1, 1) * 100;
+
             // View and projection matrices
             float4x4 viewMatrix = Transforms.LookAtLH(CameraPosition, float3(0, 1, 0), float3(0, 1, 0));
             float4x4 projectionMatrix = Transforms.PerspectiveFovLH(pi_over_4, texture.Height / (float)texture.Width, 0.01f, 20);
 
             Scene<PositionNormal> scene = new Scene<PositionNormal>();
             CreateMeshScene(scene);
+
+            BRDF[] brdfs =
+            {
+                LambertBRDF(float3(0.4f,0.5f,1f)),  // see
+                Mixture(LambertBRDF(float3(0.7f,0.7f,0.3f)), BlinnBRDF(float3(1,1,1), 70), 0.3f), // egg
+            };
 
             // Raycaster to trace rays and check for shadow rays.
             Raytracer<ShadowRayPayload, PositionNormal> shadower = new Raytracer<ShadowRayPayload, PositionNormal>();
@@ -237,16 +279,23 @@ namespace Renderer
                 attribute = attribute.Transform(context.FromGeometryToWorld);
 
                 float3 V = normalize(CameraPosition - attribute.Position);
-                float3 L = normalize(LightPosition - attribute.Position);
-                float lambertFactor = max(0, dot(attribute.Normal, L));
+                float3 L = (LightPosition - attribute.Position);
+                float d = length(L);
+                L /= d; // normalize direction to light reusing distance to light
+
+                float3 N = attribute.Normal;
+
+                float lambertFactor = max(0, dot(N, L));
 
                 // Check ray to light...
                 ShadowRayPayload shadow = new ShadowRayPayload();
                 shadower.Trace(scene,
-                    RayDescription.FromTo(attribute.Position + attribute.Normal * 0.001f, // Move an epsilon away from the surface to avoid self-shadowing 
-                    LightPosition), ref shadow);
+                    RayDescription.FromDir(attribute.Position + N * 0.001f, // Move an epsilon away from the surface to avoid self-shadowing 
+                    L), ref shadow);
 
-                payload.Color = shadow.Shadowed ? float3(0, 0, 0) : float3(1, 1, 1) * lambertFactor;
+                float3 Intensity = (shadow.Shadowed ? 0.0f : 1.0f) * LightIntensity / (d * d);
+
+                payload.Color = brdfs[context.GeometryIndex](N, L, V) * Intensity * lambertFactor;
             };
             raycaster.OnMiss += delegate (IRaycastContext context, ref MyRayPayload payload)
             {
@@ -258,7 +307,7 @@ namespace Renderer
                 for (int py = 0; py < texture.Height; py++)
                 {
                     int progress = (px * texture.Height + py);
-                    if (progress % 100 == 0)
+                    if (progress % 1000 == 0)
                     {
                         Console.Write("\r" + progress * 100 / (float)(texture.Width * texture.Height) + "%            ");
                     }
@@ -275,15 +324,22 @@ namespace Renderer
 
         static void Main(string[] args)
         {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
             // Texture to output the image.
             Texture2D texture = new Texture2D(512, 512);
 
             //SimpleRaycast(texture);
-            LitRaycast(texture);
-            //RaycastingMesh(texture);
+            //LitRaycast(texture);
+            RaycastingMesh(texture);
+
+            stopwatch.Stop();
 
             texture.Save("test.rbm");
-            Console.WriteLine("Done.");
+
+            Console.WriteLine("Done. Rendered in " + stopwatch.ElapsedMilliseconds + " ms");
         }
     }
 }
